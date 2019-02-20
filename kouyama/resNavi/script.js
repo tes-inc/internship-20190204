4fdc5676e1c2e6df810344582d70eb05980e96d3
a-kouyama <shenshanatsushi33@gmail.com>
2019-02-20 17:59:08 +0900

    近くの店舗検索機能に範囲指定を追加

--- kouyama/resNavi/script.js
+++ kouyama/resNavi/script.js
@@ -1,10 +1,29 @@
 const gnaviUrl = `https://api.gnavi.co.jp/RestSearchAPI/v3/`; // ぐるなびAPIのURL
 const keyid = 'e2d1f212bb0ddfdd774c23e22bedf31e'; // ぐるなびAPIのkeyid
 let data; // URLとして送信するオブジェクト
+let $searchName; // 店名検索に入力された文字列
+let $freeWord; // フリーワード検索に入力された文字列
+let page; // 現在のページ数を格納
+
+// pageボタンが押された時の処理
+$('#nextPage').on('click', function() {
+  ++page;
+  data.offset_page = page;
+  display(data);
+})
 
  // 検索ボタンが押された時
  $('#search').on('click', function() {
-  wordInput();
+  data = { keyid : keyid }; // 前回の検索dataを初期化
+
+  $searchName = $('#searchName').val(); // 「店名」検索フォームに入力された文字列
+  $freeWord = $('#freeWord').val(); // 「フリーワード」検索フォームに入力された文字列
+
+  if (!$searchName && !$freeWord) { // 店名とフリーワードどちらも入力されていない場合
+    alert('検索ワードを入力してください');
+    return;
+  }
+  wordInput(); // 入力された文字列をdataに格納
 
   $.ajax( { // 非同期処理
     url: gnaviUrl, // リクエスト送信先URL
@@ -16,20 +35,26 @@ let data; // URLとして送信するオブジェクト
   .done(function(data) { // ajaxの処理が成功した時、dataにその結果が入る。
     display(data);
   })
-  .fail(function() { // ajaxの処理が失敗したら。タイムアウトしたら。
-    alert('通信エラー');
-  });
+  .fail(function(jqXHR, textStatus, errorThrown) { // ajaxの処理失敗時
+    $('#searchResults').append(errorThrown); // エラー内容を表示
+  })
 })
 
 // 「近くの店舗を検索」ボタンが押された時
 $('#nearStore').on('click', function() {
-  wordInput();
+  data = { keyid : keyid }; // 前回の検索dataを初期化
 
+  $searchName = $('#searchName').val(); // 「店名」検索フォームに入力された文字列
+  $freeWord = $('#freeWord').val(); // 「フリーワード」検索フォームに入力された文字列
+  let $range = $('#range').val(); // 検索する範囲を指定する変数
+
+  wordInput(); // 入力されたデータをdataに格納
   // 現在位置を取得する
-  navigator.geolocation.getCurrentPosition(function(genzaiti) {
+  navigator.geolocation.getCurrentPosition(function(genzaiti) { // 現在地が取得できたら
 
     data.latitude = genzaiti.coords.latitude; // 現在地の緯度と経度を取得し送信するdataに入れる
     data.longitude = genzaiti.coords.longitude;
+    data.range = $range; // 検索範囲をdataに入れる
 
     $.ajax( {
       url: gnaviUrl,
@@ -42,6 +67,7 @@ $('#nearStore').on('click', function() {
       display(data); // 検索結果を表示
     })
     .fail(function(jqXHR, textStatus, errorThrown) { // ajaxの処理失敗時
+        $('#searchResults').text(''); // 前回の検索結果表示を初期化
         $('#searchResults').append(errorThrown); // エラー内容を表示
     })
   });
@@ -49,16 +75,6 @@ $('#nearStore').on('click', function() {
 
 // 入力された検索ワードをdataに入れる関数
 function wordInput() {
-  $('#searchResults').text(''); // 前回の検索結果表示を初期化
-  data = { keyid : keyid }; // 前回の検索dataを初期化
-
-  let $searchName = $('#searchName').val(); // 「店名」検索フォームに入力された文字列
-  let $freeWord = $('#freeWord').val(); // 「フリーワード」検索フォームに入力された文字列
-
-  if (!$searchName && !$freeWord) { // 店名とフリーワードどちらも入力されていない場合
-    alert('入力されていない項目があります');
-    return;
-  }
   if ($searchName && $freeWord) { // 店名とフリーワード両方入力された場合
     data.name = $searchName; // 送信するデータにプロパティnameを追加
     data.freeword = $freeWord; // 送信するデータにプロパティfreewordを追加
@@ -71,6 +87,7 @@ function wordInput() {
 
 // 受け取ったデータを表示する関数
 function display(data) {
+  $('#searchResults').text(''); // 前回の検索結果表示を初期化
   $(data.rest).each(function() { // dataのプロパティrestの要素一つ一つに対して繰り返し処理
     // 検索結果を画面に表示
     // 名前とカテゴリーを表示
